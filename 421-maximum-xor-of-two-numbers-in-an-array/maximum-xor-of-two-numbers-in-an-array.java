05ed062cd029cc1b41232a6cd9e961a0fe6bcbee
Chirag Monga <56741241+chirag-m1@users.noreply.github.com>
2026-09-07 20:59:00 +0530

    Time: 853 ms (50.11%) | Memory: 208.6 MB (74.87%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,69 @@
+class Node {
+    Node[] links = new Node[2];
+    
+    Node() {}
+
+    boolean containsKey(int bit) {
+        return links[bit] != null;
+    }
+
+    Node get(int bit) {
+        return links[bit];
+    }
+
+    void put(int bit, Node node) {
+        links[bit] = node;
+    }
+}
+
+class Trie {
+    private Node root;
+    
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(int num) {
+        Node node = root;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if(!node.containsKey(bit)) {
+                node.put(bit, new Node());
+            }
+            node = node.get(bit);
+        }
+    }
+
+    public int getMaxXOR(int num) {
+        Node node = root;
+        int maxi = 0;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if(node.containsKey(1 - bit)) {
+                maxi = maxi | (1 << i);
+                node = node.get(1 - bit);
+            }
+            else {
+                node = node.get(bit);
+            }
+        }
+        return maxi;
+    }
+}
+
+
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        Trie trie = new Trie();
+        for(int i = 0; i < nums.length; i++) {
+            trie.insert(nums[i]);
+        }
+
+        int result = 0;
+        for(int i = 0; i < nums.length; i++) {
+            result = Math.max(result, trie.getMaxXOR(nums[i]));
+        }
+
+        return result;
+    }
+}
