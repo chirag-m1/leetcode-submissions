dafec9c47c65be80052f33e587acf51a0acd3cb8
Chirag Monga <56741241+chirag-m1@users.noreply.github.com>
2026-09-15 14:53:30 +0530

    Time: 746 ms (22.40%) | Memory: 79.9 MB (21.91%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] prime = new boolean[n];
+        for(int i = 2; i < n; i++) {
+            prime[i] = true;
+        }
+
+        for(int i = 2; i < Math.sqrt(n); i++) {
+            if(prime[i]) {
+                for(int j = i * i; j < n; j = j + i) {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(prime[i]) count++;
+        }
+        return count;
+    }
+}
